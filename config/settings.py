696155d149f0bbfcb705d557973e691df2a7dce5
Your Name <you@example.com>
2026-07-02 13:26:09 +0300

    Import os in settings

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
+
 import os
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
